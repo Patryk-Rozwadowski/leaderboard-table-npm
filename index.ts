9b1443832f86976c07192db18e0d43efc7a1cfa5
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-09-05 21:11:41 +0200

    refactor: package reformat with prettier

--- index.ts
+++ index.ts
@@ -1,47 +1,45 @@
 interface LeaderboardConfig {
-    rootContainer: HTMLElement;
+   rootContainer: HTMLElement;
 
-    // TODO data types
-    data: any;
+   // TODO data types
+   data: any;
 }
 
-const Leaderboard = function ({rootContainer, data}: LeaderboardConfig): void {
-    // TODO implement event system
-    const events = [];
-    let root: HTMLElement;
+const Leaderboard = function ({ rootContainer, data }: LeaderboardConfig): void {
+   // TODO implement event system
+   const events = [];
+   let root: HTMLElement;
 
-    function mount() {
+   function mount() {
+      root = rootContainer;
+      const frag = document.createDocumentFragment();
 
-        root = rootContainer;
-        const frag = document.createDocumentFragment();
+      const headerTag = "h1";
+      const headers = ["Header First", "Header Second"];
 
-        const headerTag = "h1";
-        const headers = ["Header First", "Header Second"];
+      headers.map((header) => {
+         let tag = document.createElement(headerTag);
+         tag.textContent = header;
+         frag.appendChild(tag);
+      });
+      root.appendChild(frag);
+   }
 
-        headers.map(header => {
-            let tag = document.createElement(headerTag);
-            tag.textContent = header;
-            frag.appendChild(tag);
-        })
-        root.appendChild(frag);
-    }
+   function typeGuards() {
+      if (
+         typeof rootContainer === "undefined" ||
+         !(rootContainer instanceof HTMLElement)
+      ) {
+         throw new Error(`Expected e to be an HTMLElement, was ${typeof rootContainer}.`);
+      }
+   }
 
-    function typeGuards() {
-        if (
-            typeof rootContainer === "undefined" ||
-            !(rootContainer instanceof HTMLElement)
-        ) {
-            throw new Error(`Expected e to be an HTMLElement, was ${typeof rootContainer}.`);
-        }
-    }
+   function init(): void {
+      typeGuards();
+      mount();
+   }
 
-    function init(): void {
-        typeGuards();
-        mount();
-    }
+   init();
+};
 
-    init();
-}
-
-export default Leaderboard
-
+export default Leaderboard;
