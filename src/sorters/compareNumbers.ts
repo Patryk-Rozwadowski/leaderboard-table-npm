58f07c172d54635755e73587eeb81aa268c75df4
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-09-12 22:39:46 +0200

    refactor: created sorters

--- src/sorters/compareNumbers.ts
+++ src/sorters/compareNumbers.ts
@@ -1,3 +1,9 @@
+export enum ComparisonResult {
+   FirstBeforeSecond = -1,
+   Equal = 0,
+   SecondBeforeFirst = 1
+}
+
 function compareNumbers(a: number, b: number): number {
    return a - b;
 }
