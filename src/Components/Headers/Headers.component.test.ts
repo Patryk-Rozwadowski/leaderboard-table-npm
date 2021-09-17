eaad4e2551a255daf3406ffbb41d5e82eda7462d
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-09-18 00:28:17 +0200

    test: init tests for Headers component with mock data

---
+++ src/Components/Headers/Headers.component.test.ts
@@ -0,0 +1,21 @@
+import Headers from "./Headers.component";
+import { MOCK_HEADERS_TEXT } from "../../__mocks__/inputs";
+
+describe("Header component", () => {
+   let rootContainer: HTMLElement;
+   let headers: Headers;
+
+   beforeEach(() => {
+      rootContainer = document.createElement("div");
+      rootContainer.classList.add("root");
+   });
+
+   it("should render header texts", () => {
+      headers = new Headers(rootContainer, MOCK_HEADERS_TEXT);
+      expect(headers).toBeTruthy();
+   });
+
+   it("should not render header texts", () => {
+      headers = new Headers(rootContainer);
+   });
+});
---
+++ src/__mocks__/inputs.ts
@@ -0,0 +1,22 @@
+const MOCK_PLACE_CONTENT = [
+   {
+      place: 53,
+      content: "First"
+   },
+   {
+      place: 4,
+      content: "Second"
+   },
+   {
+      place: 8,
+      content: "8"
+   },
+   {
+      place: 56,
+      content: "Fourth"
+   }
+];
+
+const MOCK_HEADERS_TEXT = ["Header First", "Header Second"];
+
+export { MOCK_HEADERS_TEXT, MOCK_PLACE_CONTENT };
