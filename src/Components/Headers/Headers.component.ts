1dff42cca71a13cac3a13f47a664c5af2edbb2c8
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-09-18 00:29:15 +0200

    refactor: minor data handling changes

--- src/Components/Headers/Headers.component.ts
+++ src/Components/Headers/Headers.component.ts
@@ -1,32 +1,30 @@
 import PhasesState from "../../PhasesState";
 
 class Headers extends PhasesState {
-   constructor(private root: HTMLElement, private headersText: string[]) {
+   constructor(private root: HTMLElement, private headersText?: string[] | undefined) {
       super();
       this.createHeaders();
    }
 
    private createHeaders() {
-      if (!this.isHeaderExists()) {
+      console.log(`Headers exists: ${!!this.headersText}`);
+
+      if (!this.headersText) {
          return;
       }
 
       const headerContainer = document.createElement("div");
       headerContainer.classList.add("lb_headers");
 
-      this.headersText.map((headerText) => {
+      this.headersText?.map((headerText) => {
          // TODO createElement: dynamic tag defined by user
          const headerTag = document.createElement("h5");
          headerTag.textContent = headerText;
          headerTag.classList.add("lb_headers_text", "lb_col");
          headerContainer.appendChild(headerTag);
       });
-      this.root.appendChild(headerContainer);
-   }
 
-   private isHeaderExists() {
-      console.log(`Headers exists: ${!!this.headersText}`);
-      return !!this.headersText;
+      this.root.appendChild(headerContainer);
    }
 }
 
