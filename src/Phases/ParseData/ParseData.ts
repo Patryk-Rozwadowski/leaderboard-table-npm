14b7e8135dcbdf37176b915f07d36236158c44dd
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-09-29 23:49:55 +0200

    refactor: split index code with Parse phase

--- src/Phases/ParseData/ParseData.ts
+++ src/Phases/ParseData/ParseData.ts
@@ -4,18 +4,37 @@ import { Newable } from "../../common/common.types";
 
 class ParseData extends PhasesState {
    private _logger: Logger;
+   private _data: any;
+   private _userOptions: any;
+   private _rootContainer: HTMLElement;
 
-   constructor() {
+   constructor(rootContainer: HTMLElement, data: any, userOptions: any) {
       super();
 
+      this._rootContainer = rootContainer;
       this._logger = new Logger(this as unknown as Newable);
+      this._data = data;
+      this._userOptions = userOptions;
    }
 
    public execute(): void {
       this._logger.log(`Started parsing data.`);
+      if (!Array.isArray(this._data) || !this._data) {
+         this._logger.log(`Data is not defined. Pass leaderboard information.`);
+         return;
+      }
    }
 
-   private sortRows() {}
+   private typeGuards() {
+      if (
+         typeof this._rootContainer === "undefined" ||
+         !(this._rootContainer instanceof HTMLElement)
+      ) {
+         throw new Error(
+            `Expected e to be an HTMLElement, was ${typeof this._rootContainer}.`
+         );
+      }
+   }
 }
 
 export default ParseData;
--- src/index.ts
+++ src/index.ts
@@ -30,34 +30,18 @@ class Leaderboard {
    private _headers: any;
 
    // TODO refactor constructor types/params
-   constructor({ rootContainer, data, headers }: LeaderboardConfig) {
+   constructor({ rootContainer, data, headers, options }: LeaderboardConfig) {
       this._rootContainer = rootContainer;
       this._logger = new Logger(this as unknown as Newable);
-
-      if (!Array.isArray(data) || !data) {
-         this._logger.log(`Data is not defined. Pass leaderboard information.`);
-         return;
-      }
-      this._data = data;
       this._headers = headers;
-      this._phasesContext = new PhasesContext(new ParseData());
+
+      this._phasesContext = new PhasesContext(
+         new ParseData(rootContainer, data, options)
+      );
       this._phasesContext.execute();
    }
 
-   private typeGuards() {
-      if (
-         typeof this._rootContainer === "undefined" ||
-         !(this._rootContainer instanceof HTMLElement)
-      ) {
-         throw new Error(
-            `Expected e to be an HTMLElement, was ${typeof this._rootContainer}.`
-         );
-      }
-   }
-
    public init(): void {
-      this.typeGuards();
-
       this._phasesContext.transitionTo(
          new Mount(this._rootContainer, this._data, this._headers)
       );
