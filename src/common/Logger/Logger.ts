82dfc1ab5c489fd14c870488819cdd280862fe40
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-10-01 21:36:54 +0200

    feat: improved Logger

--- src/common/Logger/Logger.ts
+++ src/common/Logger/Logger.ts
@@ -7,6 +7,18 @@ class Logger {
       const contextName = this._context.constructor.name;
       console.log(`${contextName}: ${msg}`);
    }
+
+   public group(label: string): void {
+      console.group(label);
+   }
+
+   public groupEnd(): void {
+      console.groupEnd();
+   }
+
+   public error(msg: string): void {
+      console.trace(msg);
+   }
 }
 
 export default Logger;
