102f90ee397cc203bd408c28ba966820dd7fec71
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-12-08 23:39:47 +0100

    refactor: remove redudant types

--- src/common/common.types.ts
+++ src/common/common.types.ts
@@ -1,18 +1,4 @@
-import ElementCreator from "../factories/ElementCreator";
-import { ROW_CLASS_STYLE } from "../factories/row/Row";
-import { TYPOGRAPHY_STYLE_CLASS } from "../style/styleClasses/typography.enum";
-import { CONTAINER_STYLE_CLASS } from "../style/styleClasses/container.enum";
-
 type Newable = { new (...args: any): any };
-
-/*
- * Creator
- *    This type is used for any class, which is allowed to create
- *    new Leaderboard elements.
- */
-interface Creator {
-   _elementCreator: ElementCreator;
-}
 
 // Single pre parsed client column
 type SingleRowProperties = { [key: string]: string | number } | string;
@@ -27,18 +13,4 @@ type ColumnProperties = {
 
 type HeaderKey = { header: string };
 
-interface Component extends Creator {
-   create(): HTMLElement | HTMLElement[];
-}
-
-type LbCSSClass = ROW_CLASS_STYLE | CONTAINER_STYLE_CLASS | TYPOGRAPHY_STYLE_CLASS;
-
-export {
-   Newable,
-   Creator,
-   LbCSSClass,
-   Component,
-   ColumnProperties,
-   HeaderKey,
-   SingleRowProperties
-};
+export { Newable, ColumnProperties, HeaderKey, SingleRowProperties };
