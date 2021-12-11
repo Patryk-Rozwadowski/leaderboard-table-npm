47207386a1ed8e877c5d415da7552633bdc684f0
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2021-12-11 22:38:53 +0100

    refactor: Row component code quality improvements

--- src/factories/row/Row.ts
+++ src/factories/row/Row.ts
@@ -1,21 +1,21 @@
-import ElementCreator from "../ElementCreator";
+import ElementCreator, { ComponentFactory } from "../ElementCreator";
 import { Newable, SingleRowProperties } from "../../common/common.types";
 import Logger from "../../common/Logger/Logger";
 import DOMController from "../../controllers/DOMController";
-import { ROW_CLASS_STYLE } from "../../style/styleClasses/row.enum";
 import { SEMANTIC_TAGS } from "../../style/semanticTags";
 import { COMPONENT_STYLES } from "../../style/styleClasses";
 
 interface RowContainers {
-   placeContainer: HTMLElement;
    contentContainer: HTMLElement;
+   rowContainer: HTMLElement;
 }
 
-class Row {
+class Row implements ComponentFactory {
+   DOMController: DOMController;
+
    private _elementCreator: ElementCreator;
    private _rowListContainer: HTMLElement;
    private _logger: Logger;
-   private _DOMController: DOMController;
 
    constructor(
       private _rootContainer: HTMLElement,
@@ -23,53 +23,60 @@ class Row {
    ) {
       this._elementCreator = new ElementCreator();
       this._logger = new Logger(this as unknown as Newable);
-      this._DOMController = new DOMController();
+      this.DOMController = new DOMController();
    }
 
    public create(): HTMLElement {
-      this._rowListContainer = this._elementCreator.createContainer(
-         SEMANTIC_TAGS.CONTAINER_ROW,
-         COMPONENT_STYLES.ROW_LIST_CONTAINER
-      );
-
+      this._rowListContainer = this._createMainRowContainer();
       this._createRow();
       this._logger.groupEnd();
-
       return this._rowListContainer;
    }
 
-   private _createRow() {
-      const rowWrapper = this._elementCreator.createContainer(
+   private _createMainRowContainer() {
+      return this._elementCreator.createContainer(
          SEMANTIC_TAGS.CONTAINER_ROW,
-         COMPONENT_STYLES.ROW_CONTAINER
+         COMPONENT_STYLES.CONTENT_CONTAINER
       );
+   }
 
-      const { contentContainer } = this._createRowContainers();
-      const textContent = this._elementCreator.createText(
+   private _createRow() {
+      const { rowContainer, contentContainer } = this._createRowContainers();
+      const textContent = this._rowContentText();
+
+      DOMController.appendChildrenToContainer(contentContainer, textContent);
+      DOMController.appendChildrenToContainer(rowContainer, contentContainer);
+
+      this._rowListContainer.appendChild(rowContainer);
+   }
+
+   private _rowContentText(): HTMLElement {
+      return this._elementCreator.createText(
          SEMANTIC_TAGS.PRIMARY_TEXT,
          this._rowData.toString()
       );
-
-      DOMController.appendChildrenToContainer(contentContainer, textContent);
-      DOMController.appendChildrenToContainer(rowWrapper, contentContainer);
-
-      this._rowListContainer.appendChild(rowWrapper);
    }
 
    private _createRowContainers(): RowContainers {
-      const placeContainer = this._elementCreator.createContainer(
-         SEMANTIC_TAGS.CONTAINER_ROW,
-         COMPONENT_STYLES.CONTAINER
-      );
+      const rowContainer = this._createRowContainer();
+      const contentContainer = this._createRowContentContainer();
 
-      const contentContainer = this._elementCreator.createContainer(
-         SEMANTIC_TAGS.CONTAINER_ROW,
-         COMPONENT_STYLES.CONTAINER
-      );
+      return { rowContainer, contentContainer };
+   }
 
-      return { placeContainer, contentContainer };
+   private _createRowContainer(): HTMLElement {
+      return this._elementCreator.createContainer(
+         SEMANTIC_TAGS.CONTAINER_ROW,
+         COMPONENT_STYLES.ROW_CONTAINER
+      );
+   }
+
+   private _createRowContentContainer(): HTMLElement {
+      return this._elementCreator.createContainer(
+         SEMANTIC_TAGS.CONTAINER_ROW,
+         COMPONENT_STYLES.CONTENT_CONTAINER
+      );
    }
 }
 
-export { ROW_CLASS_STYLE };
 export default Row;
