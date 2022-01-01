af0f6dbbd39e0a8bedba6880dbfaaf498bada106
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2022-01-01 23:00:05 +0100

    test: add ClientInputVerificator tests

---
+++ src/common/ClientInputVerificator/__test__/ClientInputVerification.test.ts
@@ -0,0 +1,40 @@
+import ClientInputVerification from "../ClientInputVerification";
+import { PreParsedLeaderboardData } from "../../../index";
+
+describe("ClientInputVerification", () => {
+   let clientVerification: ClientInputVerification;
+   beforeEach(() => {
+      clientVerification = new ClientInputVerification();
+   });
+
+   describe("isRootContainerValid", () => {
+      it("Should fail", () => {
+         const wrongData = "" as unknown as HTMLElement;
+         expect(() => clientVerification.isRootContainerValid(wrongData)).toThrow(
+            "Expected root element to be an HTMLElement, was string."
+         );
+      });
+
+      it("Should work", () => {
+         const div = document.createElement("div");
+         expect(clientVerification.isRootContainerValid(div)).toBe(true);
+      });
+   });
+
+   describe("isDataStructureValid", () => {
+      it("Should fail - data not array", () => {
+         const failData = {} as unknown as PreParsedLeaderboardData[];
+         expect(clientVerification.isDataStructureValid(failData)).toBe(false);
+      });
+
+      it("Should work - correct data structure", () => {
+         const correctData = [
+            {
+               name: "Bob",
+               points: 20
+            }
+         ] as unknown as PreParsedLeaderboardData[];
+         expect(clientVerification.isDataStructureValid(correctData)).toBe(true);
+      });
+   });
+});
