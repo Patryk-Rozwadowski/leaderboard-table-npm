c81d242f4a6526bd373065fe77d2ed9cbcc4dc68
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2022-01-11 21:47:08 +0100

    refactor: remove styleClasses / semanticTags  from style folder

--- src/style/styleClasses/container.enum.ts
+++
@@ -1,9 +0,0 @@
-enum CONTAINER_STYLE_CLASS {
-   ROOT_CONTAINER = "lb",
-   COLUMN_CONTAINER = "lb_col",
-   HEADERS_CONTAINER = "lb_headers_container",
-   CELL_CONTAINER = "lb_cell_container",
-   CELL = "lb_cell"
-}
-
-export { CONTAINER_STYLE_CLASS };
